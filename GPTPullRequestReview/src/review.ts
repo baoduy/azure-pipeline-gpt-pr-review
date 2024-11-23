f924687e4d9231b6e10201cafb63895f090abc58
Steven Hoang <hoangbaoduy@gmail.com>
2024-11-23 11:16:11 +0800

    update packages and enhance the OpenAPI

--- GPTPullRequestReview/src/review.ts
+++ GPTPullRequestReview/src/review.ts
@@ -1,15 +1,19 @@
-import fetch from 'node-fetch';
-import { git } from './git';
-import { OpenAIApi } from 'openai';
-import { addCommentToPR } from './pr';
-import { Agent } from 'https';
+import { git } from "./git";
+import { OpenAI } from "openai";
+import { addCommentToPR } from "./pr";
+import { Agent } from "https";
 import * as tl from "azure-pipelines-task-lib/task";
 
-export async function reviewFile(targetBranch: string, fileName: string, httpsAgent: Agent, apiKey: string, openai: OpenAIApi | undefined, aoiEndpoint: string | undefined) {
+export async function reviewFile(
+  targetBranch: string,
+  fileName: string,
+  httpsAgent: Agent,
+  openai: OpenAI,
+) {
   console.log(`Start reviewing ${fileName} ...`);
 
-  const defaultOpenAIModel = 'gpt-3.5-turbo';
-  const patch = await git.diff([targetBranch, '--', fileName]);
+  const defaultOpenAIModel = "gpt-3.5-turbo";
+  const patch = await git.diff([targetBranch, "--", fileName]);
 
   const instructions = `Act as a code reviewer of a Pull Request, providing feedback on possible bugs and clean code issues.
         You are provided with the Pull Request changes in a patch format.
@@ -18,46 +22,27 @@ export async function reviewFile(targetBranch: string, fileName: string, httpsAg
         As a code reviewer, your task is:
                 - Review only added, edited or deleted lines.
                 - If there's no bugs and the changes are correct, write only 'No feedback.'
-                - If there's bug or uncorrect code changes, don't write 'No feedback.'`;
+                - If there's bug or incorrect code changes, don't write 'No feedback.'`;
 
   try {
     let choices: any;
 
-    if (openai) {
-      const response = await openai.createChatCompletion({
-        model: tl.getInput('model') || defaultOpenAIModel,
-        messages: [
-          {
-            role: "system",
-            content: instructions
-          },
-          {
-            role: "user",
-            content: patch
-          }
-        ],
-        max_tokens: 500
-      });
+    const response = await openai.chat.completions.create({
+      model: tl.getInput("model") || defaultOpenAIModel,
+      messages: [
+        {
+          role: "system",
+          content: instructions,
+        },
+        {
+          role: "user",
+          content: patch,
+        },
+      ],
+      max_tokens: 500,
+    });
 
-      choices = response.data.choices
-    }
-    else if (aoiEndpoint) {
-      const request = await fetch(aoiEndpoint, {
-        method: 'POST',
-        headers: { 'api-key': `${apiKey}`, 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          max_tokens: 500,
-          messages: [{
-            role: "user",
-            content: `${instructions}\n, patch : ${patch}}`
-          }]
-        })
-      });
-
-      const response = await request.json();
-
-      choices = response.choices;
-    }
+    choices = response.choices;
 
     if (choices && choices.length > 0) {
       const review = choices[0].message?.content as string;
@@ -68,8 +53,7 @@ export async function reviewFile(targetBranch: string, fileName: string, httpsAg
     }
 
     console.log(`Review of ${fileName} completed.`);
-  }
-  catch (error: any) {
+  } catch (error: any) {
     if (error.response) {
       console.log(error.response.status);
       console.log(error.response.data);
